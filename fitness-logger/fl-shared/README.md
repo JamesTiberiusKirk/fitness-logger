dbfa8d1a18ae535abbb94029cd257a48068d3395
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-02-18 18:17:39 +0000

    Update

--- fitness-logger/fl-shared/README.md
+++
@@ -1,1 +0,0 @@
-# This is a package for the shared code for the micro-services backend for fitness
--- fitness-logger/fl-shared/src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as Conf from './lib/models/conf.model';
-import * as Env from './lib/Env';
-import * as Logger from './lib/Logger';
-import { initStopHandler } from './lib/ProcSigHandlers';
-import * as JwtWrapper from './lib/JwtWrapper';
-
-export { Conf, Env, Logger, JwtWrapper, initStopHandler }
